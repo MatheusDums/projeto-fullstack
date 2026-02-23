1fa74180c13e2fef3137741c7ebb1f4379759ac5
MatheusDums <dumss@icloud.com>
2026-02-23 17:24:56 -0300

    commit react - aula 2.4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import Logo from './componentes/Logo';
+import Perfil from './imagens/perfil.svg';
+import Sacola from './imagens/sacola.svg';
 
+const textoOpcoes = ['Categorias', 'Minha Estante', 'Favoritos'];
+const icones = [Perfil, Sacola];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Logo></Logo>
+        <ul className='opcoes'>
+          {textoOpcoes.map((texto, index) => (
+            <li key={index} className='opcao'><p>{texto}</p></li>
+          ))}
+        </ul>
+
+        <ul className='icones'>
+          {icones.map((icone, index) => (
+            <li key={index} className='icone'><img src={icone} alt="Ícone" /></li>
+          ))}
+        </ul>
       </header>
     </div>
   );
--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -4,7 +4,11 @@ import './estilo.css';
 function Logo() {
     return (
         <div className='logo'>
-            <img src={logo} alt="Logo" />
+            <img 
+                src={logo} 
+                alt="Logo" 
+                className='logo-img'
+            />
             <p><strong>Alura</strong> Books</p>
         </div>
     )
